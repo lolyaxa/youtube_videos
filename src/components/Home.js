4a6e777aa4603def86512493e18124c388513256
lolyaxa <oliatabakova@yandex.com>
2020-09-27 23:57:27 +0300

    установлен prettier

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,106 +1,117 @@
-import React, { useState, useEffect } from 'react';
-import './Home.css';
-import { Paginator } from './Paginator';
-import { SearchBar } from './SearchBar';
+import React, { useState, useEffect } from "react";
+import "./Home.css";
+import { Paginator } from "./Paginator";
+import { SearchBar } from "./SearchBar";
 import { NavLink } from "react-router-dom";
-import createBrowserHistory from 'history/createBrowserHistory';
+import createBrowserHistory from "history/createBrowserHistory";
 
 const history = createBrowserHistory();
-const CashArr = [];
+const localStorage = [];
 
 function Home() {
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
   const [response, setResponse] = useState();
   const [activePage, setActivePage] = useState(1);
   const isLoading = response === undefined;
-  useEffect(
-    () => {
-      async function f() {
-        let url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&key=AIzaSyDxDqDp328CXFtTj6XYVwIa4WKUH-YT6UI&q=';
-        if (inputValue === '') {
-          url += 'обзор%20игры%20метрон';
-        } else {
-          url += inputValue;
-        }
-        let responses = await fetch(url);
-        let commits = await responses.json();
-        setResponse(commits.items);
-        
+  useEffect(() => {
+    async function f() {
+      let url =
+        "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&key=AIzaSyDxDqDp328CXFtTj6XYVwIa4WKUH-YT6UI&q=";
+      if (inputValue === "") {
+        url += "обзор%20игры%20метрон";
+      } else {
+        url += inputValue;
       }
-      f();
-    },
-  []);
+      let responses = await fetch(url);
+      let commits = await responses.json();
+      setResponse(commits.items);
+    }
+    f();
+  }, []);
 
-  if(!isLoading) {
+  if (!isLoading) {
     const videoList = [];
-    let videoArr = response.slice(((activePage -1) * 10), (activePage * 10 - 1))
+    let videoArr = response.slice((activePage - 1) * 10, activePage * 10 - 1);
     for (let i in videoArr) {
       videoList.push(
-        <li><NavLink to={`/video/${videoArr[i].id.videoId}`}>
-          <div className='VideoButton'>
-            <img className='VideoPreview' src={videoArr[i].snippet.thumbnails.medium.url}/>
-            <div className='VideoTitle'> {videoArr[i].snippet.title}</div>
-            <div className='ChannelTitle'>{videoArr[i].snippet.channelTitle}</div>
-            <div className='VideoDescription'> {videoArr[i].snippet.description.slice(0, 99)}</div>            
-          </div>
-        </NavLink></li>)
-    }
-        return (
-          <div>
-            <SearchBar
-              searchFilterOnChange={(e) =>
-              {
-                setInputValue(e.target.value)
-              }}
-              value={inputValue}
-              onSearchClick={() =>
-              {
-                async function f() {
-                  let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&key=AIzaSyDxDqDp328CXFtTj6XYVwIa4WKUH-YT6UI&q=${inputValue}`;
-                  let responses = await fetch(url);
-                  let commits = await responses.json();
-                  setResponse(commits.items);
-                }
-                f();
-                CashArr.push(inputValue);
-              }}
-            />
-
-            <div className='SearchHistory'>
-              {CashArr.map(function(a) {
-                return <div
-                  onClick={() =>
-                    {
-                      async function f() {
-                        let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&key=AIzaSyDxDqDp328CXFtTj6XYVwIa4WKUH-YT6UI&q=${a}`;
-                        let responses = await fetch(url);
-                        let commits = await responses.json();
-                        setResponse(commits.items);
-                      }
-                    f();
-                    setInputValue(a);
-                    for(var i = CashArr.length - 1; i >= 0; i--) {
-                      if(CashArr[i] === a) {
-                         CashArr.splice(i, 1);
-                      }
-                    }
-                    CashArr.unshift(a);
-                    }}>{a}</div>
-              })}
+        <li>
+          <NavLink to={`/video/${videoArr[i].id.videoId}`}>
+            <div className="VideoButton">
+              <img
+                className="VideoPreview"
+                src={videoArr[i].snippet.thumbnails.medium.url}
+              />
+              <div className="VideoTitle"> {videoArr[i].snippet.title}</div>
+              <div className="ChannelTitle">
+                {videoArr[i].snippet.channelTitle}
+              </div>
+              <div className="VideoDescription">
+                {" "}
+                {videoArr[i].snippet.description.slice(0, 99)}
+              </div>
             </div>
+          </NavLink>
+        </li>
+      );
+    }
+    return (
+      <div>
+        <SearchBar
+          searchFilterOnChange={(e) => {
+            setInputValue(e.target.value);
+          }}
+          value={inputValue}
+          onSearchClick={() => {
+            async function f() {
+              let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&key=AIzaSyDxDqDp328CXFtTj6XYVwIa4WKUH-YT6UI&q=${inputValue}`;
+              let responses = await fetch(url);
+              let commits = await responses.json();
+              setResponse(commits.items);
+            }
+            f();
+            localStorage.push(inputValue);
+          }}
+        />
 
-            <div>{videoList}</div>
+        <div className="SearchHistory">
+          {localStorage.map(function (a) {
+            return (
+              <div
+                onClick={() => {
+                  async function f() {
+                    let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&key=AIzaSyDxDqDp328CXFtTj6XYVwIa4WKUH-YT6UI&q=${a}`;
+                    let responses = await fetch(url);
+                    let commits = await responses.json();
+                    setResponse(commits.items);
+                  }
+                  f();
+                  setInputValue(a);
+                  for (var i = localStorage.length - 1; i >= 0; i--) {
+                    if (localStorage[i] === a) {
+                      localStorage.splice(i, 1);
+                    }
+                  }
+                  localStorage.unshift(a);
+                }}
+              >
+                {a}
+              </div>
+            );
+          })}
+        </div>
 
-            <Paginator
-              total={response.length}
-              perPage={10}
-              activePage={activePage}
-              onSelect={(pageNumber) => {
-                setActivePage(pageNumber);
-              }}
-            />
-          </div>
-        );
+        <div>{videoList}</div>
+
+        <Paginator
+          total={response.length}
+          perPage={10}
+          activePage={activePage}
+          onSelect={(pageNumber) => {
+            setActivePage(pageNumber);
+          }}
+        />
+      </div>
+    );
   } else {
     return <div>Загрузка...</div>;
   }
--- src/components/VideoPage.js
+++ src/components/VideoPage.js
@@ -1,7 +1,7 @@
-import './VideoPage.css';
-import React, { useState, useEffect } from 'react';
+import "./VideoPage.css";
+import React, { useState, useEffect } from "react";
 import { NavLink, useParams } from "react-router-dom";
-import createBrowserHistory from 'history/createBrowserHistory';
+import createBrowserHistory from "history/createBrowserHistory";
 
 const history = createBrowserHistory();
 
@@ -9,33 +9,51 @@ function RepositoryPage() {
   let { id } = useParams();
   const [response, setResponse] = useState();
   const isLoading = response === undefined;
-  useEffect(
-    () => {
-      async function f() {
-        const responses = await fetch(
-          `https://www.googleapis.com/youtube/v3/videos?part=snippet&part=contentDetails&key=AIzaSyAGpl45i0RE7QA3TsQGGp3_0GiQ3jKCFTM&id=${id}`, )
-        let commits = await responses.json();
-        console.log(id);
-        setResponse(commits.items);
-      }
-      f();
-    },
-  []);
+  useEffect(() => {
+    async function f() {
+      const responses = await fetch(
+        `https://www.googleapis.com/youtube/v3/videos?part=snippet&part=contentDetails&key=AIzaSyDxDqDp328CXFtTj6XYVwIa4WKUH-YT6UI&id=${id}`
+      );
+      let commits = await responses.json();
+      console.log(id);
+      setResponse(commits.items);
+    }
+    f();
+  }, []);
 
   if (!isLoading) {
     console.log(response);
     return (
       <div>
-        <li><NavLink to={`/`}>Назад</NavLink></li>
-        <div className='Background'>
-          <iframe src={`http://www.youtube.com/embed/${id}`} width='560' height='315' frameborder='0' allowfullscreen></iframe>
-          <div className='VideoTitle'> {response[0].snippet.title}</div>
-          <a target="_blank" href={`https://www.youtube.com/channel/${response[0].snippet.channelId}`}>{response[0].snippet.channelTitle}</a>
-          <div className='VideoDuration'> {response[0].contentDetails.duration}</div>
-          <div className='VideoDescription'> {response[0].snippet.description}</div>
+        <li>
+          <NavLink to={`/`}>Назад</NavLink>
+        </li>
+        <div className="Background">
+          <iframe
+            src={`http://www.youtube.com/embed/${id}`}
+            width="560"
+            height="315"
+            frameborder="0"
+            allowfullscreen
+          ></iframe>
+          <div className="VideoTitle"> {response[0].snippet.title}</div>
+          <a
+            target="_blank"
+            href={`https://www.youtube.com/channel/${response[0].snippet.channelId}`}
+          >
+            {response[0].snippet.channelTitle}
+          </a>
+          <div className="VideoDuration">
+            {" "}
+            {response[0].contentDetails.duration}
+          </div>
+          <div className="VideoDescription">
+            {" "}
+            {response[0].snippet.description}
+          </div>
         </div>
       </div>
-    )
+    );
   } else {
     return <div>Загрузка...</div>;
   }
